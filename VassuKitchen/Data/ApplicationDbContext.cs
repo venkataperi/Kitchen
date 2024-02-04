a1d099f080280bdfbddbb8d833cbde96ef05bc0d
Venkata Peri <periv@algonquincollege.com>
2024-02-03 19:17:24 -0500

    Added category table to database

---
+++ VassuKitchen/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VassuKitchen.Model;
+
+namespace VassuKitchen.Data;
+
+public class ApplicationDbContext :DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+
+    }
+
+    public DbSet<Category> Category { get; set; }
+
+}
--- VassuKitchen/Program.cs
+++ VassuKitchen/Program.cs
@@ -1,12 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using VassuKitchen.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
+
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
